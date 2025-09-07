16f5b3680787a11a59177c652f1af864a31d1bb6
Indrajeet Yadav <indrajeetyadav993648@gmail.com>
2025-09-07 22:45:09 +0530

    Facade-report  Implemented Successfully

--- design-pattern-assignments/adapter-csv-import/src/com/example/imports/App.java
+++ design-pattern-assignments/adapter-csv-import/src/com/example/imports/App.java
@@ -7,10 +7,7 @@ public class App {
         if (args.length == 0)
             throw new IllegalArgumentException("path required");
         Path csv = Path.of(args[0]);
-        // TODO: Implement and use CsvProfileImporter here
         ProfileImporter importer = new CsvProfileImporter(new NaiveCsvReader(), new ProfileService());
-        // ProfileImporter importer = new CsvProfileImporter(new NaiveCsvReader(), new
-        // ProfileService());
         int n = importer.importFrom(csv);
         System.out.println("Imported " + n + " profiles");
         System.out.println("Import OK (wire the adapter to complete).");
